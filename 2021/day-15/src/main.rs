83c70d26bf938dcc9d7a8bc172001ab2ef4b4c80
Dave Teare <dave@agilebits.com>
2021-12-22 10:09:59 -0500

    Busting out Dijkstra to ⭐️⭐️ day 15 🙌

--- 2021/day-15/src/main.rs
+++ 2021/day-15/src/main.rs
@@ -1,14 +1,17 @@
-use std::collections::HashMap;
 use std::fmt;
+
+use pathfinding::prelude::dijkstra;
+
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+struct Pos(usize, usize);
 
 struct Puzzle {
     map: Vec<Vec<usize>>,
-    cache: HashMap<(usize,usize),usize>,
 }
 
 impl Puzzle {
-    fn parse(input: &str) -> Puzzle {
-        println!("Parsing <{}>", input);
+    fn parse_part1(input: &str) -> Puzzle {
+        // println!("Parsing <{}>", input);
         let mut map: Vec<Vec<usize>> = Vec::new();
 
         for (_i, line_str) in input.trim().split("\n").enumerate() {
@@ -17,7 +20,7 @@ impl Puzzle {
                 continue;
             }
 
-            let mut row:Vec<usize> = Vec::new();
+            let mut row: Vec<usize> = Vec::new();
             for (_j, c) in trimmed.chars().enumerate() {
                 let risk = c.to_digit(10).unwrap();
                 row.push(risk.try_into().unwrap());
@@ -25,71 +28,68 @@ impl Puzzle {
             map.push(row);
         }
 
-        let cache: HashMap<(usize,usize),usize> = HashMap::new();
-        Puzzle { map, cache }
+        Puzzle { map }
     }
 
-    fn lowest_risk(&mut self) -> usize {
-        self.path_walker(0, 0)  - self.map[0][0]
-    }
+    fn parse_part2(input: &str) -> Puzzle {
+        let puzzle_template = Self::parse_part1(input);
 
-    /// (row,col) is the starting position. Think of it as the remaining square after navigating (and starting from) this position. Assume no switchbacks so the shortest path is either the one to the right or the one going down. 
-    fn path_walker(&mut self, row:usize, col:usize) -> usize {
-        //println!("@path_walker ({},{})", row, col);
-        if row > self.map.len()-1 && col > self.map[0].len()-1 {
-            panic!("Coding error. Should have avoided recursing this deep. ({}, {})", row, col);
-        }
+        let template_height = puzzle_template.map.len();
+        let template_width = puzzle_template.map[0].len();
 
-        if row == self.map.len() - 1 && col == self.map[0].len()-1 {
-            return self.map[row][col];
-        }
+        let mut map: Vec<Vec<usize>> = Vec::with_capacity(template_height * 5);
+        map.resize(template_height * 5, vec![0; template_width * 5]);
 
-        if row >= self.map.len() - 1 {
-            return self.map[row][col] + self.path_walker(row, col+1);
-        }
-        if col >= self.map[0].len() - 1 {
-            return self.map[row][col] + self.path_walker(row+1, col);
-        }
-
-
-        let right_risk:usize;
-        {
-            let cached_right = self.cache.get_mut(&(row, col+1));
-            match cached_right {
-                Some(val) => {
-                    right_risk = *val;
-                },
-                None => {
-                    right_risk = self.path_walker(row, col+1);
+        for section_row in 0..5 {
+            for section_col in 0..5 {
+                for i in 0..template_height {
+                    for j in 0..template_width {
+                        let mut value = puzzle_template.map[i][j] + section_row + section_col;
+                        if value > 9 {
+                            value = value % 9;
+                        }
+                        map[section_row * template_height + i][section_col * template_width + j] =
+                            value;
+                    }
                 }
             }
         }
 
-        let cached_down = self.cache.get_mut(&(row+1, col));
-        let down_risk:usize;
-        match cached_down {
-            Some(val) => {
-                down_risk = *val;
-            },
+        Puzzle { map }
+    }
+
+    fn lowest_risk(&mut self) -> usize {
+        let dest = Pos(self.map[0].len() - 1, self.map.len() - 1);
+        let result = dijkstra(&Pos(0, 0), |p| self.neighbours(p), |p| *p == dest);
+
+        match result {
+            Some(path) => path.1,
             None => {
-                down_risk = self.path_walker(row+1, col);
+                panic!("No path found");
             }
         }
+    }
 
-        let result:usize;
-        if right_risk < down_risk {
-            result = self.map[row][col] + right_risk
+    /// Given a position on the map, return the neighbouring cells and the required "weight" to get there.
+    fn neighbours(&self, pos: &Pos) -> Vec<(Pos, usize)> {
+        let &Pos(x, y) = pos;
+        let mut cells: Vec<Pos> = Vec::new();
+
+        if x > 0 {
+            cells.push(Pos(x - 1, y));
         }
-        else if down_risk < right_risk {
-            result = self.map[row][col] + down_risk
-        }
-        else {
-            println!("⚠️ Point ({},{})={} Tricky map has equal risk right and down. Choosing right.", row, col, self.map[row][col]);
-            result = self.map[row][col] + right_risk
+        if x < self.map[0].len() - 1 {
+            cells.push(Pos(x + 1, y));
         }
 
-        self.cache.insert((row,col), result);
-        result
+        if y > 0 {
+            cells.push(Pos(x, y - 1));
+        }
+        if y < self.map.len() - 1 {
+            cells.push(Pos(x, y + 1));
+        }
+
+        cells.into_iter().map(|p| (p, self.map[p.1][p.0])).collect()
     }
 
     fn part_1(&mut self) -> usize {
@@ -97,7 +97,7 @@ impl Puzzle {
     }
 
     fn part_2(&mut self) -> usize {
-        0
+        self.lowest_risk()
     }
 }
 
@@ -123,10 +123,12 @@ pub fn read_stdin() -> Result<String, std::io::Error> {
 }
 
 fn main() -> Result<(), std::io::Error> {
-    let mut puzzle = Puzzle::parse(&read_stdin()?);
+    let input = &read_stdin()?;
+    let mut puzzle1 = Puzzle::parse_part1(input);
+    println!("Part 1: {}", puzzle1.part_1());
 
-    println!("Part 1: {}", puzzle.part_1());
-    println!("Part 2:\n{}", puzzle.part_2());
+    let mut puzzle2 = Puzzle::parse_part2(input);
+    println!("Part 2: {}", puzzle2.part_2());
 
     Ok(())
 }
@@ -151,49 +153,93 @@ mod test {
 
     #[test]
     fn baby_steps() {
-        let mut puzzle = super::Puzzle::parse(r#"
+        let mut puzzle = super::Puzzle::parse_part1(
+            r#"
             11
             91
-        "#);
+        "#,
+        );
         assert_eq!(2, puzzle.lowest_risk());
 
-
-        puzzle = super::Puzzle::parse(r#"
+        puzzle = super::Puzzle::parse_part1(
+            r#"
             19
             11
-        "#);
+        "#,
+        );
         assert_eq!(2, puzzle.lowest_risk());
 
-
-        puzzle = super::Puzzle::parse(r#"
+        puzzle = super::Puzzle::parse_part1(
+            r#"
             129
             129
             839
-        "#);
+        "#,
+        );
         assert_eq!(15, puzzle.lowest_risk());
 
-
-        puzzle = super::Puzzle::parse(r#"
+        puzzle = super::Puzzle::parse_part1(
+            r#"
             111
             275
             839
-        "#);
+        "#,
+        );
         assert_eq!(16, puzzle.lowest_risk());
+
+        assert_eq!(2, puzzle.neighbours(&Pos(0, 0)).len());
+        assert_eq!(2, puzzle.neighbours(&Pos(2, 0)).len());
+        assert_eq!(2, puzzle.neighbours(&Pos(0, 2)).len());
+        assert_eq!(2, puzzle.neighbours(&Pos(2, 2)).len());
+        assert_eq!(4, puzzle.neighbours(&Pos(1, 1)).len());
+
+        assert_eq!(
+            3,
+            puzzle
+                .neighbours(&Pos(0, 0))
+                .iter()
+                .fold(0, |acc, n| acc + n.1)
+        );
+
+        assert_eq!(
+            11,
+            puzzle
+                .neighbours(&Pos(1, 1))
+                .iter()
+                .fold(0, |acc, n| acc + n.1)
+        );
+
+        assert_eq!(
+            8,
+            puzzle
+                .neighbours(&Pos(2, 2))
+                .iter()
+                .fold(0, |acc, n| acc + n.1)
+        );
+
+        puzzle = super::Puzzle::parse_part1(
+            r#"
+            19999
+            19111
+            11191"#,
+        );
+        assert_eq!(8, puzzle.lowest_risk());
     }
 
     #[test]
     fn part_1() {
-        let mut puzzle = super::Puzzle::parse(SAMPLE);
-
-        println!("Parsed puzzle:\n{}", puzzle);
+        let mut puzzle = super::Puzzle::parse_part1(SAMPLE);
 
         assert_eq!(puzzle.map.len(), 10);
         assert_eq!(40, puzzle.part_1());
     }
 
-    // #[test]
-    // fn part_2() {
-    //     let mut puzzle = super::Puzzle::parse(SAMPLE);
-    //     assert_eq!(2188189693529, puzzle.part_2());
-    // }
+    #[test]
+    fn part_2() {
+        let mut puzzle = super::Puzzle::parse_part2(SAMPLE);
+
+        assert_eq!(puzzle.map.len(), 10 * 5); // 5 times larger in both dimensions
+        assert_eq!(puzzle.map[0].len(), 10 * 5);
+        assert_eq!(315, puzzle.part_2());
+    }
 }
