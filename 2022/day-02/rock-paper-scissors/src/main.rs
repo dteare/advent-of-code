f7042585d4962ffd5c791b44b4f572b3947984a4
Dave Teare <dave@agilebits.com>
2022-12-03 14:32:11 -0500

    Day 2 ⭐️⭐️

--- 2022/day-02/rock-paper-scissors/src/main.rs
+++ 2022/day-02/rock-paper-scissors/src/main.rs
@@ -7,6 +7,19 @@ enum Choice {
     Scissors,
 }
 
+#[derive(Debug, Clone, PartialEq)]
+enum Ending {
+    Win,
+    Lose,
+    Draw,
+}
+
+// How to interpret the X, Y, Z encoding within the input
+enum Encoding {
+    WhatToPlay, // star 1
+    HowItEnds,  // star 2
+}
+
 struct Tournament {
     rounds: Vec<Round>,
 }
@@ -33,23 +46,75 @@ impl FromStr for Choice {
     }
 }
 
-fn parse_choices(input: &str) -> (Choice, Choice) {
+impl FromStr for Ending {
+    type Err = ();
+
+    fn from_str(input: &str) -> Result<Ending, Self::Err> {
+        match input {
+            "X" => Ok(Ending::Lose),
+            "Y" => Ok(Ending::Draw),
+            "Z" => Ok(Ending::Win),
+            _ => Err(()),
+        }
+    }
+}
+
+fn winning_play(theirs: &Choice) -> Choice {
+    match theirs {
+        Choice::Rock => Choice::Paper,
+        Choice::Paper => Choice::Scissors,
+        Choice::Scissors => Choice::Rock,
+    }
+}
+
+fn losing_play(theirs: &Choice) -> Choice {
+    match theirs {
+        Choice::Rock => Choice::Scissors,
+        Choice::Paper => Choice::Rock,
+        Choice::Scissors => Choice::Paper,
+    }
+}
+
+fn drawing_play(theirs: &Choice) -> Choice {
+    match theirs {
+        Choice::Rock => Choice::Rock,
+        Choice::Paper => Choice::Paper,
+        Choice::Scissors => Choice::Scissors,
+    }
+}
+
+fn parse_choices(input: &str, mode: &Encoding) -> (Choice, Choice) {
     let mut parts = input.split(" ");
     let theirs = Choice::from_str(parts.next().unwrap()).unwrap();
-    let mine = Choice::from_str(parts.next().unwrap()).unwrap();
 
-    (theirs, mine)
+    let my_str = parts.next().unwrap();
+
+    match mode {
+        Encoding::WhatToPlay => {
+            let mine = Choice::from_str(my_str).unwrap();
+            (theirs, mine)
+        }
+        Encoding::HowItEnds => {
+            let ending = Ending::from_str(my_str).unwrap();
+            let mine = match ending {
+                Ending::Win => winning_play(&theirs),
+                Ending::Lose => losing_play(&theirs),
+                Ending::Draw => drawing_play(&theirs),
+            };
+            (theirs, mine)
+        }
+    }
 }
 
 impl Tournament {
-    fn parse(input: &str) -> Tournament {
-        println!("Parsing <{}>", input);
+    fn parse(input: &str, mode: Encoding) -> Tournament {
+        //println!("Parsing <{}>", input);
         let mut rounds: Vec<Round> = Vec::new();
 
         for (_i, line_str) in input.trim().split("\n").enumerate() {
             let _trimmed = line_str.trim();
 
-            let (theirs, mine) = parse_choices(line_str);
+            let (theirs, mine) = parse_choices(line_str, &mode);
 
             rounds.push(Round {
                 theirs: theirs,
@@ -60,18 +125,12 @@ impl Tournament {
         Tournament { rounds }
     }
 
-    fn part_1(&mut self) -> usize {
-        let mut score = 0;
-
-        for round in self.rounds.iter() {
-            score = score + round.score();
-        }
-
-        score
+    fn part_1(&self) -> usize {
+        self.rounds.iter().map(|r| r.score()).sum()
     }
 
-    fn part_2(&mut self) -> usize {
-        2
+    fn part_2(&self) -> usize {
+        self.rounds.iter().map(|r| r.score()).sum()
     }
 }
 
@@ -118,9 +177,11 @@ pub fn read_stdin() -> Result<String, std::io::Error> {
 }
 
 fn main() -> Result<(), std::io::Error> {
-    let mut tourney = Tournament::parse(&read_stdin()?);
-
+    let input = &read_stdin()?;
+    let tourney = Tournament::parse(input, Encoding::WhatToPlay);
     println!("Part 1: {}", tourney.part_1());
+
+    let tourney = Tournament::parse(input, Encoding::HowItEnds);
     println!("Part 2: {}", tourney.part_2());
 
     Ok(())
@@ -139,7 +200,7 @@ C Z
 
     #[test]
     fn baby_steps() {
-        let tourney = super::Tournament::parse(SAMPLE);
+        let tourney = super::Tournament::parse(SAMPLE, Encoding::WhatToPlay);
 
         assert_eq!(tourney.rounds.len(), 3);
         assert_eq!(tourney.rounds[0].theirs, Choice::Rock);
@@ -157,13 +218,13 @@ C Z
 
     #[test]
     fn part_1() {
-        let mut tourney = super::Tournament::parse(SAMPLE);
+        let mut tourney = super::Tournament::parse(SAMPLE, Encoding::WhatToPlay);
         assert_eq!(15, tourney.part_1());
     }
 
     #[test]
     fn part_2() {
-        let mut tourney = super::Tournament::parse(SAMPLE);
-        assert_eq!(222, tourney.part_2());
+        let mut tourney = super::Tournament::parse(SAMPLE, Encoding::HowItEnds);
+        assert_eq!(12, tourney.part_2());
     }
 }
